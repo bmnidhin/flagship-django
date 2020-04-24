92932caf00b179f6504424f2822a47a72ed2cdc0
Nidhin BM <nidhinbm.bm@gmail.com>
2020-04-24 17:09:47 +0530

    everything fine

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_home, name='post_home'),
+    path('allposts', views.post_list, name='post_list'),
     path('post/<slug>/', views.post_detail, name='post_detail'),
     # path('post/<int:pk>/', views.post_detail, name='post_detail'),
 #    path('posts/<slug:slug>', views.post_detail, name='post_detail')
